b0a4468369af4fa024fe4cb7448b3b1aa6291cde
Cristian <cristianespinosabarajas@gmail.com>
2022-09-02 15:45:56 -0500

    primer commit del test

---
+++ main.js
@@ -0,0 +1,9 @@
+const p = document.querySelector('p')
+const h1 = document.querySelector('h1')
+const parrafito = document.querySelector('.parrafito')
+const pid = document.querySelector('#pid')
+const input = document.querySelector('input')
+
+console.log({p, parrafito, pid, input, h1})
+
+pid.innerHTML = ""
